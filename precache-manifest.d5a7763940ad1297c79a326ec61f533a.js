2b33196f0a014c8de0d7c2811e6be4141e340bea
Wayan Danyael Eco <wayaneco29@gmail.com>
2019-12-19 19:13:44 +0800

    Updates

---
+++ precache-manifest.d5a7763940ad1297c79a326ec61f533a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb29318da042ca86f8a0b3e4756377e4",
+    "url": "/98labs/index.html"
+  },
+  {
+    "revision": "80875bfbe7f574947668",
+    "url": "/98labs/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "f232cc8d15ae5fc9f9fc",
+    "url": "/98labs/static/css/main.932233de.chunk.css"
+  },
+  {
+    "revision": "80875bfbe7f574947668",
+    "url": "/98labs/static/js/2.efde47e4.chunk.js"
+  },
+  {
+    "revision": "f232cc8d15ae5fc9f9fc",
+    "url": "/98labs/static/js/main.45ec35c7.chunk.js"
+  },
+  {
+    "revision": "2e8256d5e0b04945479c",
+    "url": "/98labs/static/js/runtime-main.f849030a.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/98labs/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "d8cfbd9cb70da140467d07abc7823840",
+    "url": "/98labs/static/media/banner.d8cfbd9c.webp"
+  }
+]);
